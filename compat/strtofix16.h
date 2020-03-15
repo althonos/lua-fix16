d3902ea3639e07099f7d38031532d9b1f7b17f85
Martin Larralde <martin.larralde@ens-paris-saclay.fr>
2020-03-15 16:42:46 +0100

    Add compatibility functions for `libfixmath` in `compat` folder

---
+++ compat/strtofix16.h
@@ -0,0 +1,9 @@
+#ifndef strtofix16_h
+#define strtofix16_h
+
+#include <fix16.h>
+
+fix16_t strtofix16(const char *nptr, char** endptr);
+size_t fix16tostr(fix16_t value, char *buf, int decimals);
+
+#endif
